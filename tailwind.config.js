998c68f567a46740edeba5dab72d27b9e8cdde39
egirldevs <emily@Bunni.me>
2023-10-30 23:43:06 -0400

    darkmode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,19 @@ module.exports = {
                 "primary-50": "#F4F2FF",
                 "success-700": "#027A48",
                 "success-50": "#ECFDF3",
+                darkmode: {
+                    "primary": "#d657bc",
+                    "secondary": "#3b8e00",
+                    "accent": "#4464af",
+                    "neutral": "#2a2d37",
+                    "base-100": "#222e49",
+                    "info": "#8599f4",
+                    "success": "#56e6cc",
+                    "warning": "#f48d1f",          
+                    "error": "#f70840",
+                }
             },
         },
     },
-    plugins: [],
-};
+}
+    

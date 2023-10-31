0aff1e027e381ded77b6a44c26058e33d13a626b
dashwastakenx <contact.pear.casa@gmail.com>
2023-10-31 22:55:43 +0000

    teeesst

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import {redirect} from "next/navigation";
 import Link from "next/link";
 
 export default async function BlogPage() {
-    const route = `http://100.67.254.17:7777/api/blog/posts`
+    const route = `http://https://api.nopox.xyz/api/blog/posts`
 
     let posts: any[] = []
     await fetch(route).then(async it => {
--- app/blog/post.tsx
+++ app/blog/post.tsx
@@ -7,7 +7,7 @@ const fetcher = async (...args: [string, RequestInit?]) => await fetch(...args).
 })
 
 export function Post(key: String) {
-    const route = `http://100.67.254.17:7777/api/blog/post/${key}`
+    const route = `http://https://api.nopox.xyz/api/blog/post/${key}`
     console.log(route)
 
     const {data, error, isValidating} = useSWR(route, fetcher)

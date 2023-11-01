f9625dd661d7331c568f9ba0fb7eb53b22401147
egirldevs <emily@Bunni.me>
2023-10-31 23:25:12 -0400

    User Page but fix error k thanks

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,26 +1,48 @@
+"use client"
+
 import {auth, clerkClient} from "@clerk/nextjs";
 import {redirect} from "next/navigation";
-import {UserDetails} from "./details";
+import React from 'react';
 
-export default async function UsersPage() {
+interface UserDetailsProps {
+    user: any; // Replace 'any' with the actual type of the user
+}
+
+export function UserDetails({ user }: UserDetailsProps) {
+    // Component implementation
+    return (
+        <div>
+            {/* Display user details here */}
+            <p>{user.name}</p>
+        </div>
+    );
+}
+export default function UsersPage() {
     const {userId} = auth();
 
     if (!userId) {
         redirect("/");
     }
 
-    const user = await clerkClient.users.getUser(userId);
+    const [users, setUsers] = React.useState<any[]>([]);
+
+    React.useEffect(() => {
+        const fetchUsers = async () => {
+            const userList = await clerkClient.users.getUserList();
+            setUsers(userList);
+        };
+
+        fetchUsers();
+    }, []);
 
     return (
         <div className="px-6 md:px-12">
-            {user && (
-                <>
-                    <h1 className="text-3xl font-semibold">
-                        Users
-                    </h1>
-                    <UserDetails/>
-                </>
-            )}
+            <h1 className="text-3xl font-semibold">
+                Users
+            </h1>
+            {users.map((user) => (
+                <UserDetails key={user.id} user={user} />
+            ))}
         </div>
     );
 }
